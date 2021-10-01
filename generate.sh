f563aacac732a1c57764394c81777e9ff73b7f11
dinhtp <dinhtp@smartosc.com>
2021-10-01 23:13:03 +0700

    Generate Gateway

--- generate.sh
+++ generate.sh
@@ -2,17 +2,18 @@
 
 set -e
 
+# shellcheck disable=SC2230
 PROTOC=$(which protoc)
 
-# Generate protobufs types all the dir. Exclude hidden directories such as .git
+# Generate proto buf types all the dir. Exclude hidden directories such as .git
 shopt -u dotglob
 find ./* -prune -type d ! -name third_party | while read -r d; do
     ${PROTOC} \
     -I=. \
-    -I=${GOPATH}/src \
-    -I=${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/ \
-    -I=${GOPATH}/src/github.com/gogo/protobuf/protobuf \
-    -I=${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway/ \
+    -I="${GOPATH}"/src \
+    -I="${GOPATH}"/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/ \
+    -I="${GOPATH}"/src/github.com/gogo/protobuf/protobuf \
+    -I="${GOPATH}"/src/github.com/grpc-ecosystem/grpc-gateway/ \
     --gogo_out=plugins=grpc,\
 Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,\
 Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,\
@@ -22,7 +23,7 @@ Mgoogle/protobuf/field_mask.proto=github.com/gogo/protobuf/types,\
 Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,\
 Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,\
 Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types:\
-${GOPATH}/src/ \
+"${GOPATH}"/src/ \
     --govalidators_out=gogoimport=true,\
 Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,\
 Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,\
@@ -32,8 +33,18 @@ Mgoogle/protobuf/field_mask.proto=github.com/gogo/protobuf/types,\
 Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,\
 Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,\
 Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types:\
-${GOPATH}/src/ \
+"${GOPATH}"/src/ \
+    --grpc-gateway_out=allow_patch_feature=false,\
+Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/empty.proto=github.com/gogo/protobuf/types,\
+Mgoogle/api/annotations.proto=github.com/gogo/googleapis/google/api,\
+Mgoogle/protobuf/field_mask.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,\
+Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types:\
+"${GOPATH}"/src/ \
     --swagger_out=third_party/OpenAPI/ \
-${d}/*.proto
+"${d}"/*.proto
 
 done
